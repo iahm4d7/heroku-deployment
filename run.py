48c32ce8b15c953d5faee625c1230c19e17729d3
Ahmed <iahm4d7@gmail.com>
2020-10-16 08:36:06 +0300

    A

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
-from app import app as application
+from app import app
 from db import db
 
-db.init__app(application)
+db.init_app(app)
 
 
-@application.before_first_request
+@app.before_first_request
 def create_tables():
     db.create_all()
